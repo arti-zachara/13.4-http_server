c4f05ab3959a6c83e057a49de52e6346ad1ef1cf
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-04-26 13:17:52 +0200

    Change index.js file to read index.html file

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 var http = require("http");
+var fs = require("fs");
 
 var server = http.createServer();
+
 server.on("request", function(request, response) {
-  response.write("Hello world!");
-  response.end();
+  if (request.method === "GET" && request.url === "/") {
+    fs.readFile("./index.html", "utf-8", function(err, data) {
+      // read index.html file and respond with positive outcome
+      response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      if (!err) response.write(data);
+      // err handling
+      response.end();
+    });
+  } else {
+    fs.readFile("./404.jpg", function(err, data) {
+      response.statusCode = 404;
+      if (!err) response.write(data);
+      // err handling
+      response.end();
+    });
+  }
 });
-server.listen(9000);
+
+server.listen(8080);
 // listens for events
