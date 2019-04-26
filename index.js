1dd327aa57c35b728d662ebb7f006192f464d604
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-04-26 13:35:02 +0200

    Add 404.png and index.html to be displayed as a response

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ server.on("request", function(request, response) {
     fs.readFile("./index.html", "utf-8", function(err, data) {
       // read index.html file and respond with positive outcome
       response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      // if ok, then prepare a header
       if (!err) response.write(data);
       // err handling
       response.end();
     });
   } else {
-    fs.readFile("./404.jpg", function(err, data) {
+    fs.readFile("./404.png", function(err, data) {
       response.statusCode = 404;
       if (!err) response.write(data);
       // err handling
